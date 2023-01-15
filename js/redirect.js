303584a5452ec543081e7fdce218bac063c73bd6
sparkasparkabumen <122673583+sparkasparkabumen@users.noreply.github.com>
2023-01-15 22:35:02 +0300

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,4 +1,2 @@
-if (location.search && !isNaN(parseInt(location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length)))){
-alert('ok');
-Object.assign(document.createElement('a'), { target: '_blank', href: 'https://t.me/r1414m_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length)}).click();
-}
+if (location.search && !isNaN(parseInt(location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length))))
+location.href = 'https://t.me/r1414m_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
