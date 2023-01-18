4d0be30d2a5ae3b729b2927f63fbdd0310d9fc2a
sparkasparkabumen <122673583+sparkasparkabumen@users.noreply.github.com>
2023-01-18 21:53:30 +0300

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,2 +1,3 @@
 if (location.search && !isNaN(parseInt(location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length))))
-location.href = 'https://t.me/r1414m_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
+for (let button of document.querySelector('.btn'))
+button.href = 'https://t.me/r1414m_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
