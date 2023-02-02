3d03325d91b1bc7108d325c198d66909609942c8
sparkasparkabumen <122673583+sparkasparkabumen@users.noreply.github.com>
2023-02-02 20:37:16 +0600

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,6 +1,6 @@
 var interval;
 if (location.search && !isNaN(parseInt(location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length)))){
-for (let button of document.querySelectorAll('.btn')) button.href = 'https://t.me/r1414m_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
+for (let button of document.querySelectorAll('.btn')) button.href = 'https://t.me/ubtexpert_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
 for (let link of document.querySelectorAll('.link')){
     link.parentElement.href = '#home';
     link.parentElement.target = '';
