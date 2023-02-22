023e73380b86008d5ae3a6c7452713589cbe6cf4
sparkasparkabumen <122673583+sparkasparkabumen@users.noreply.github.com>
2023-02-22 23:00:10 +0600

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,23 +1,25 @@
 var interval;
 if (location.search && !isNaN(parseInt(location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length)))){
-for (let button of document.querySelectorAll('.btn')) button.href = 'https://t.me/ubtexpert_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
-for (let link of document.querySelectorAll('.link')){
-    link.parentElement.href = '#home';
-    link.parentElement.target = '';
-    link.onclick = () => {
-        clearInterval(interval);
-        interval = setInterval(() => {
-            let buttons = document.querySelectorAll('.btn');
-            if (buttons[0].style.backgroundColor == 'rgb(255, 255, 255)')
-                buttons[0].style.backgroundColor = '#00E77F';
-            else
-                buttons[0].style.backgroundColor = '#ffffff';
-    
-            if (buttons[1].style.backgroundColor == 'rgb(255, 255, 255)')
-                buttons[1].style.backgroundColor = '#00E77F';
-            else
-                buttons[1].style.backgroundColor = '#ffffff';
-        }, 500);
+    for (let button of document.querySelectorAll('.btn')) button.href = 'https://t.me/ubtexpert_bot?start=' + location.search.substring(2, location.search.includes('&') ? location.search.indexOf('&') : location.search.length);
+    document.querySelector('#price').innerHTML = '12 500 тг';
+    document.querySelector('#newprice').innerHTML = '10 000 тг';
+    for (let link of document.querySelectorAll('.link')){
+        link.parentElement.href = '#home';
+        link.parentElement.target = '';
+        link.onclick = () => {
+            clearInterval(interval);
+            interval = setInterval(() => {
+                let buttons = document.querySelectorAll('.btn');
+                if (buttons[0].style.backgroundColor == 'rgb(255, 255, 255)')
+                    buttons[0].style.backgroundColor = '#00E77F';
+                else
+                    buttons[0].style.backgroundColor = '#ffffff';
+
+                if (buttons[1].style.backgroundColor == 'rgb(255, 255, 255)')
+                    buttons[1].style.backgroundColor = '#00E77F';
+                else
+                    buttons[1].style.backgroundColor = '#ffffff';
+            }, 500);
+        }
     }
-}
 }
